7185aa8e20412542c91482aee16629cb5fb538c0
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-08-08 22:09:19 +0000

    Upload this version for debugging tomorrow

---
+++ test/README.md
@@ -0,0 +1,1 @@
+This directory contains the test files and will not be evaluated
---
+++ test/myprintf.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: pointer to acharacter string, composed of zero or
+ * more directives.
+ *
+ * Description: The conversion specifiers to be handles are c, s and %.
+ * Return: The number of characters printed, exclusind the null byte
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	const char *fp;
+	char *sp;
+	int *ip, charcount;
+
+	charcount = 0;
+	va_start(ap, format);
+	for (fp = format; *fp; fp++)
+	{
+		if (*fp != '%')
+		{
+			write(1, fp, 1);
+			continue;
+		}
+		switch (*++fp)
+		{
+		case 'c':
+			*ip = va_arg(ap, int);
+			write(1, &ip, 1);
+			break;
+		case 's':
+			for (sp = va_arg(ap, char *); *sp; sp++)
+				write(1, sp, 1);
+			break;
+		default:
+			write(1, fp, 1);
+			break;
+		}
+		charcount++;
+	}
+	return charcount;
+}
